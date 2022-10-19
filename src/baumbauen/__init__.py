dc2998ee5dcd5607a9a750a95a021ce4663c46aa
jkahn <james.kahn@kit.edu>
2022-10-19 14:22:09 +0200

    Added utils src

---
+++ src/baumbauen/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = 'unknown'
+finally:
+    del get_distribution, DistributionNotFound
+
+from . import utils
+from . import losses
+from . import metrics
+from . import layers
+from . import models
+from . import ignite
+from . import config
+from . import data
